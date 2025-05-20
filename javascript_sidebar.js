e2c021b87a451339c43735fe77c44db479e939e6
Vincent-hubz <markcent0199@gmail.com>
2025-05-20 16:07:42 +0800

    Update javascript_sidebar.js

--- javascript_sidebar.js
+++ javascript_sidebar.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function() {
                     <li><a href="womens_new_arrival.html">WOMENS</a></li>
                     <li><a href="kids_new_arrival.html">KIDS</a></li>
                     <li><a href="blogpage.html">BLOG</a></li>
-                    <li><a href="cart.html">CART</a></li>
+                    <li><a href="CART_PAGE.html">CART</a></li>
                     <li><a href="customer_service.html">CUSTOMER SERVICE</a></li>
                 </ul>
             </div>
